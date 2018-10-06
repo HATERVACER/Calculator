8a04b7a6e0d69ff178de05b00cba768e6f1aa9fe
HATERVACER <40624048+HATERVACER@users.noreply.github.com>
2018-10-06 14:53:26 +0300

    Update CalculatorRU.py

--- CalculatorRU.py
+++ CalculatorRU.py
@@ -1,31 +1,44 @@
+#Модули которые будут импортированы
+#Математический модуль для корней, синусов, косинусов, тангенсов. 
 import math
+#Время для выхода
 import time
+#Цикл
 while True:
+	#Первая строка которая будет выведена
 	num = input("Напиши любой знак вычисления, если значение будет равно нулю процесс завершится(+, -, /, *, //, **, sqrt(корень), sin, cos, tan, hypot: ")
-	if num == ("0"):
+	#Выход
+	f num == ("0"):
 		print ("Процесс завершится через 3 секунд")
 		time.sleep(3)
 		break
+	#Проверка операций
 	if num in ("+, -, /, *, //, **, sqrt, sin, cos, tan, hypot"):
+		#Первая операция(+)
 		if num == '+':
 			x = float(input("Первое число= "))
 			y = float(input("Второе число= "))
 			print((x+y))
+		#Вторая операция(-)
 		elif num == '-':
 			x = float(input("Первое число= "))
 			y = float(input("Второе число= "))
 			print((x-y))
+		#Третья операция(умножение)
 		elif num == '*':
 			x = float(input("Первое число= "))
 			y = float(input("Второе число= "))
 			print((x*y))
+		#Четвертая
 		elif num == '/':
 			x = float(input("Первое число= "))
 			y = float(input("Второе число= "))
+			#Проверка на ошибки в написании
 			try:
 				print((x/y))
 			except ZeroDivisionError:
 				print("Вот ты скажи мне, нафига делить на ноль, А?!")
+		#Деление с остатком
 		elif num == '//':
 			x = float(input("Первое число= "))
 			y = float(input("Второе число= "))
@@ -34,10 +47,12 @@ while True:
 				print((x%y))
 			except ZeroDivisionError:
 				print("Деление на ноль!")
+		#Степень
 		elif num == '**':
 			x = float(input("Первое число= "))
 			y = float(input("Второе число= "))
 			print((x**y))
+		#Корень
 		elif num == 'sqrt':
 			try:
 				x = float(input("Число из которого будет браться корень: "))
@@ -45,24 +60,29 @@ while True:
 				print((math.sqrt(x)))
 			except ZeroDivisionError:
 				print("Деление на ноль!")
+		#Синус
 		elif num == 'sin':
 			try:
 				x = float(input("A: "))
 				print((math.sin(x)))
 			except ValueError:
 				print("Не обманывай меня, я крутой искусственный интелект!")
+		#Косинус
 		elif num == 'cos':
 			try:
 				x = float(input("A: "))
 				print((math.cos(x)))
+			#Проверка на буквы
 			except ValueError:
 				print("Не обманывай меня, я крутой искусственный интелект!")
+		#Тангенс
 		elif num == 'tan':
 			try:
 				x = float(input("A: "))			
 				print((math.tan(x)))
 			except ValueError:
 				print("Не обманывай меня, я крутой искусственный интелект!")
+		#Гипотенуза
 		elif num == 'hypot':
 			try:
 				x = float(input("A: "))
@@ -70,5 +90,6 @@ while True:
 				print((math.hypot(x, y)))
 			except ValueError:
 				print("Не обманывай меня, я крутой искусственный интелект!")
+	#Произойдет если в num не будет значения операции 
 	else:
 		print("Неправильное значение!")
