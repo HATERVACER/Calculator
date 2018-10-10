17b11d4b82d6d143a09c1dc55784f952998797c0
HATERVACER <40624048+HATERVACER@users.noreply.github.com>
2018-10-10 14:44:48 +0300

    Новые функции
    Добавил почти все что знаю и не знаю.

--- CalculatorRU.py
+++ CalculatorRU.py
@@ -5,15 +5,16 @@ import math
 import time
 #Цикл
 while True:
-	#Первая строка которая будет выведена
-	num = input("Напиши любой знак вычисления, если значение будет равно нулю процесс завершится(+, -, /, *, //, **, sqrt(корень), sin, cos, tan, hypot): ")
+	#Первая строка которая будет выведена 
+	print('Напиши любой знак вычисления, если значение будет равно нулю процесс завершится(+, -, /, *, //, **, sqrt(корень), sin, cos, tan, hypot, log, log1p, log10, log2, degress, radians, asin, acon, atan, atan2, sinh, cosh, tanh, asinh, acosh, ')	
+	num = input("atanh, gamma, lgamma, Scircle(Площадь круга), Ssquad(Площадь квадрата), S(Площадь), Pcircle(Периметр круга, P(Периметр прямоугольника), Psquad(Периметр квадрата)): ")
 	#Выход
 	if num == ("0"):
 		print ("Процесс завершится через 3 секунды")
 		time.sleep(3)
 		break
 	#Проверка операций
-	if num in ("+, -, /, *, //, **, sqrt, sin, cos, tan, hypot"):
+	if num in ("+, -, /, *, //, **, sqrt, sin, cos, tan, hypot, log, log1p, log10, log2, degrees, radians, asin, acon, atan, atan2, sinh, cosh, tanh, asinh, acosh, atanh, gamma, lgamma, Scircle, Ssquad, S, Pcircle, P, Psquad"):
 		#Первая операция(+)
 		if num == '+':
 			x = float(input("Первое число= "))
@@ -90,6 +91,105 @@ while True:
 				print((math.hypot(x, y)))
 			except ValueError:
 				print("Не обманывай меня, я крутой искусственный интелект!")
-	#Произойдет если в num не будет значения операции 
+		# Логарифмирование
+		elif num == 'log':
+			a = float(input("Число: "))
+			print(math.log(a))
+		# Натуральное логарифмирование
+		elif num == 'log1p':
+			a = float(input("Число: "))
+			print(math.log1p(a))
+		# Логарифм A по основанию 10
+		elif num == 'log10':
+			a = float(input("Число: "))
+			print(math.log10(a))
+		# Логарифм A по основанию 2
+		elif num == 'log2':
+			a = float(input("Число: "))
+			print(math.log2(a))
+		# Конвертирует радианы в градусы
+		elif num == 'degress':
+			a = float(input("Радианы: "))
+			print(math.degrees(a))
+		# Конвертирует градусы в радианы
+		elif num == 'radians':
+			a = float(input("Градусы: "))
+			print(math.radians(a))
+		# Арксинус в радианах
+		elif num == 'asin':
+			a = float(input("A: "))
+			print(math.asin(a))
+		# Арккосинус в радианах
+		elif num == 'acos':
+			a = float(input("A: "))
+			print(math.acos(a))
+		# Арктангенс в радианах
+		elif num == 'atan':
+			a = float(input("A: "))
+			print(math.atan(a))
+		# Арктангенс в радианах, с учетом четверти, в которой находится точка
+		elif num == 'atan2':
+			a = float(input("A: "))
+			b = float(input("B: "))
+			print(math.atan2(a, b))
+		# Вычесляет гиперболический синус
+		elif num == 'sinh':
+			a = float(input("A: "))
+			print(math.sinh(a))
+		# Вычесляет гиперболический косинус
+		elif num == 'cosh':
+			a = float(input("A: "))
+			print(math.cosh(a))
+		# Вычесляет гиперболический тангенс
+		elif num == 'tanh':
+			a = float(input("A: "))
+			print(math.tanh(a))
+		# Вычесляет обратный гиперболический синус
+		elif num == 'asinh':
+			a = float(input("A: "))
+			print(math.asinh(a))
+		# Вычесляет обратный гиперболический косинус
+		elif num == 'acosh':
+			a = float(input("A: "))
+			print(math.acosh(a))
+		# Вычесляет обратный гиперболический тангенс
+		elif num == 'atanh':
+			a = float(input("A: "))
+			print(math.atanh(a))
+		# Гамма-функция
+		elif num == 'gamma':
+			a = float(input("A: "))
+			print(math.gamma(a))
+		# Натуральный логарифм гамма-функции 
+		elif num == 'lgamma':
+			a = float(input("A: "))
+			print(math.lgamma(a))
+		# Площадь круга
+		elif num == 'Scircle':
+			R = float(input('Радиус: '))
+			print(math.pi - R)
+		# Площадь квадрата
+		elif num == 'Ssquad':
+			a = float(input('A: '))
+			print(a * a)
+		# Площадь прямоугольника
+		elif num == 'S':
+			a = float(input('A: '))
+			b = float(input('B: '))
+			print(a * b)
+		# Периметр круга
+		elif num == 'Pcircle':
+			R = float(input('Радиус: '))
+			print(6.28318530718 * R)
+		# Периметр квадрата
+		elif num == 'Psquad':
+			a = float(input('A: '))
+			print(a * 4)
+		# Периметр прямоугольника
+		elif num == 'P':
+			a = float(input('A: '))
+			b = float(input('B: '))
+			print((a + b) * 2)
+ 	# Произойдет если в num не будет значения операции 
 	else:
 		print("Неправильное значение!")
